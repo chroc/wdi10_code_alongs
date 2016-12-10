9278ae2ca098ddbed33b4314645e0bf13556e323
Kasun <kasun.maldeni@gmail.com>
2016-12-10 02:52:21 +0000

    sinatra stock app

---
+++ sinatra_app/server.rb
@@ -0,0 +1,17 @@
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'yahoofinance'
+
+set :bind, '0.0.0.0'
+set :port, 3000
+
+get '/' do
+  @stock_code = params[:stock] 
+  @result = {}
+  if @stock_code != ""
+    @result = YahooFinance::get_quotes(YahooFinance::StandardQuote, @stock_code) 
+  end
+  erb :index 
+end
+
